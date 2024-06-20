f497064ebbe9dc53040b607aa94a9b693636e802
Chase Coleman <colem2cr@dukes.jmu.edu>
2024-06-20 17:00:01 -0400

    bed buttons

--- runthenumbers/src/App.js
+++ runthenumbers/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  useEffect(() => {
+    console.log(selectedButton);
+  }, [selectedButton]);
+
+  const handleButtonClick = (buttonIndex) => {
+    setSelectedButton(buttonIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App bg-gray-400">
+      <h1 className="text-3xl font-bold underline">
+        RunTheNumbers
+      </h1> 
+      <div className="flex space-x-4 justify-center">
+        <button className={`btn btn-info ${selectedButton === 1 ? 'bg-gray-800' : ''}`}onClick={() => handleButtonClick(1)}>1</button>
+        <button className={`btn btn-info ${selectedButton === 2 ? 'bg-gray-800' : ''}`}onClick={() => handleButtonClick(2)}>2</button>
+        <button className={`btn btn-info ${selectedButton === 3 ? 'bg-gray-800' : ''}`}onClick={() => handleButtonClick(3)}>3</button>
+        <button className={`btn btn-info ${selectedButton === 4 ? 'bg-gray-800' : ''}`}onClick={() => handleButtonClick(4)}>4</button>
+      </div>
+    </div> 
   );
 }
 
